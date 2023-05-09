e03b07bba0a5cb794a5a1c9d7110991240a09213
Satoru Nabeshima <snabeshi@chicagobooth.edu>
2023-05-09 16:41:54 +0000

    Street to Coordinates done

--- app/controllers/geo_controller.rb
+++ app/controllers/geo_controller.rb
@@ -6,9 +6,24 @@ class GeoController < ApplicationController
 
   def lookup_coordinates
     @street_address = params.fetch("user_street_address")
-    xxx
-    @lattitude = params.fetch("user_street_address")
-    @longitude = params.fetch("user_street_address")
+    gmaps_token = ENV.fetch("GMAPS_KEY")
+    gmaps_api_endpoint = "https://maps.googleapis.com/maps/api/geocode/json?address=#{@street_address}&key=#{gmaps_token}"
+    
+    require "open-uri"
+
+    raw_response = URI.open(gmaps_api_endpoint).read
+
+    require "json"
+
+    parsed_response = JSON.parse(raw_response)
+    results_array = parsed_response.fetch("results")
+    first_result = results_array.at(0)
+
+    geo = first_result.fetch("geometry")
+    location = geo.fetch("location")
+
+    @latitude = location.fetch("lat")
+    @longitude = location.fetch("lng")
     render({:template => "geo_templates/street_results.html.erb"})
   end
 
